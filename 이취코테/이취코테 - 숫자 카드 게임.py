d0b08e8d8ceda06f7bbba9b6ab4f5b0e374620b7
Lee-Sunho <sunho9889@naver.com>
2022-01-03 17:38:31 +0900

    이취코테 chapter3 greedy 실전문제 3

---
+++ 이취코테/이취코테 - 숫자 카드 게임.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+data = [list(map(int, input().split())) for i in range(n)]
+
+select = 0
+min_values = [min(data[i]) for i in range(n)]
+result = max(min_values)
+print(result)
