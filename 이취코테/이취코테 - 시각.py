b8ac5d4ca6ed5ae49c89858f9ea9cb2e7536b398
Lee-Sunho <sunho9889@naver.com>
2022-01-04 19:11:01 +0900

    이취코테 chapter4 구현 예제 2

---
+++ 이취코테/이취코테 - 시각.py
@@ -0,0 +1,16 @@
+n = int(input())
+cnt = 0
+
+for i in range(1 + n):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                cnt += 1
+
+print(cnt)
+                
+            
+
+
+        
+        
