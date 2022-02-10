a05480e64a04aed294bc1e82f16a3db3cd514a22
unknown <sunho9889@soongsil.ac.kr>
2022-02-11 00:41:05 +0900

    Baekjoon 14501: 퇴사

--- Baekjoon/14501.py
+++ Baekjoon/14501.py
@@ -1,19 +1,18 @@
 n = int(input())
 data = []
-data.append((0, 0))
+
 for i in range(n):
     time, benefit = map(int, input().rstrip().split())
     data.append((time, benefit))
 
 dp = [0] * (n + 1)
-for i in range(n, 0, -1):
-    if i == n and data[i][0] == 1:
-        dp[i] = data[i][1]
-
+max_value = 0
+for i in range(n - 1, -1, -1):
     if i + data[i][0] <= n:
-        dp[i] = dp[i + data[i][0]] + data[i][1]
+        dp[i] = max(dp[i + data[i][0]] + data[i][1], max_value)
+        max_value = dp[i]
 
     else:
-        dp[i] = data[i][1]
+        dp[i] = max_value
 
-print(max(dp))
+print(max_value)
