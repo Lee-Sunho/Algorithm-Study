4a1cb2345bf1f7d01b2f0763de0a9998464a6685
unknown <sunho9889@soongsil.ac.kr>
2022-02-14 23:09:54 +0900

    Baekjoon 12015: 가장 긴 증가하는 부분 수열 2

---
+++ Baekjoon/12015.py
@@ -0,0 +1,16 @@
+import imp
+import sys
+from bisect import bisect_left
+n = int(sys.stdin.readline())
+data = list(map(int, sys.stdin.readline().rstrip().split()))
+
+dp = [data[0]]
+for i in range(1, n):
+    if data[i] > dp[-1]:
+        dp.append(data[i])
+
+    else:
+        idx = bisect_left(dp, data[i])
+        dp[idx] = data[i] # 수열이 유지되는 위치의 원소와 교환
+
+print(len(dp))
