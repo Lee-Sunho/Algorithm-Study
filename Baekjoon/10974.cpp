9830f2034ba4cd2590ca188ef715af3192905b8c
unknown <sunho9889@soongsil.ac.kr>
2022-08-01 03:20:29 +0900

    Baekjoon 10974: 모든 순열

---
+++ Baekjoon/10974.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v;
+    for(int i = 1; i <= n; i++){
+        v.push_back(i);
+    }
+
+    do{
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+    } while(next_permutation(v.begin(), v.end()));
+    return 0;
+}
