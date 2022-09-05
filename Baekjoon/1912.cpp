bf11fbff505bc9dca6e3a1e2941373ecc5b9d315
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-09-06 01:23:10 +0900

    Baekjoon 1912: 연속합

---
+++ Baekjoon/1912.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+int dp[100006];
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    v.push_back(0); // index 0 초기화
+    for(int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(dp[i-1] + v[i] > v[i]){
+            dp[i] = dp[i-1] + v[i];
+        }
+        else{
+            dp[i] = v[i];
+        }
+    }
+    cout << *max_element(dp + 1, dp+n+1);
+}
