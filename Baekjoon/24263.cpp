b40bf371b7d8dc612c77812769b3465fd4da337e
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-03-02 23:56:22 +0900

    Baekjoon 24263: 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ Baekjoon/24263.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> n;
+  cout << n
+       << "\n";
+  cout << "1"
+       << "\n";
+}
