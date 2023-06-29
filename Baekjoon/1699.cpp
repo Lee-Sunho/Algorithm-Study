cb793862a448e1962b1ad8a25adeef2dbafe4f76
Lee-Sunho <sunho9889@naver.com>
2023-06-30 02:39:05 +0900

    Baekjoon 1699: 제곱수의 합

---
+++ Baekjoon/1699.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[100006];
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    dp[i] = i;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j * j <= i; j++) {
+      dp[i] = min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+  cout << dp[n];
+}
