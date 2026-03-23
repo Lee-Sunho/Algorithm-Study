447f1653bedbdb87088fe420f97c0d645dde6d13
Lee-Sunho <sunho9889@naver.com>
2026-03-23 16:21:06 +0900

    14888: 연산자 끼워넣기

--- Baekjoon/14888.cpp
+++ Baekjoon/14888.cpp
@@ -1,64 +1,65 @@
 #include <iostream>
 using namespace std;
 
-int n;
-int num[11];
-int op[4];
-int max_res = -100000001;
-int min_res = 100000001;
+int n, num[11], op[4];
+int max_res = -1000000000;
+int min_res = 1000000001;
 
-void func(int cur, int res){
-    if (cur == n){
-        if(res > max_res){
-            max_res = res;
-        }
-        if(res < min_res){
-            min_res = res;
-        }
+int calc(int num1, int num2, int op) {
+    int ret = 0;
+    switch (op) {
+        case 0:
+            ret = num1 + num2;
+            break;
+
+        case 1:
+            ret = num1 - num2;
+            break;
+        case 2:
+            ret = num1 * num2;
+            break;
+
+        case 3:
+            ret = num1 / num2;
+            break;
+
+        default:
+            break;
+    }
+
+    return ret;
+}
+
+void func(int depth, int cur) {
+    if (depth == n) {
+        max_res = max(max_res, cur);
+        min_res = min(min_res, cur);
         return;
     }
 
-    for(int j = 0; j < 4; j++){
-        if(op[j] != 0){
-            op[j]--;
-            switch (j){
-                case 0:
-                    func(cur+1, res + num[cur]);
-                    op[j]++;
-                    break;
-
-                case 1:
-                    func(cur+1, res - num[cur]);
-                    op[j]++;
-                    break;
-
-                case 2:
-                    func(cur+1, res * num[cur]);
-                    op[j]++;
-                    break;
-
-                case 3:
-                    func(cur+1, res / num[cur]);
-                    op[j]++;
-                    break;
-            }
+    for (int i = 0; i < 4; i++) {
+        if (op[i]) {
+            op[i]--;
+            func(depth + 1, calc(cur, num[depth], i));
+            op[i]++;
         }
     }
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     cin >> n;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> num[i];
     }
 
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         cin >> op[i];
     }
+
     func(1, num[0]);
-    cout << max_res << "\n";
-    cout << min_res;
+
+    cout << max_res << "\n" << min_res;
 }
