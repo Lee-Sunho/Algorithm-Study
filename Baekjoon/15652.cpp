84c2bcedc490e757d76e2d08b5cb759e086a27be
Lee-Sunho <sunho9889@naver.com>
2026-03-25 01:43:18 +0900

    15652: N과 M (4)

---
+++ Baekjoon/15652.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, ret[10];
+
+void func(int depth, int idx) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) cout << ret[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = idx; i <= n; i++) {
+        ret[depth] = i;
+        func(depth + 1, i);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+
+    func(0, 1);
+
+    return 0;
+}
