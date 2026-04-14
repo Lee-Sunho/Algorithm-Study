13424afcf220fcbb841639cc6434202653e9d9e1
Lee-Sunho <sunho9889@naver.com>
2026-04-14 18:58:38 +0900

    10971: 외판원 순회 2

---
+++ Baekjoon/10971.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, arr[11][11], min_cost = 1e9, visited[11];
+
+void func(int cur, int cnt, int cost) {
+    if (cnt == n) {
+        if (arr[cur][0] == 0) return;
+        min_cost = min(min_cost, cost + arr[cur][0]);
+        return;
+    }
+
+    if (cost > min_cost) return;
+
+    for (int i = 1; i < n; i++) {
+        if (visited[i] || arr[cur][i] == 0) continue;
+        visited[i] = 1;
+        func(i, cnt + 1, cost + arr[cur][i]);
+        visited[i] = 0;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    visited[0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (visited[i] || arr[0][i] == 0) continue;
+        visited[i] = 1;
+        func(i, 2, arr[0][i]);
+        visited[i] = 0;
+    }
+
+    cout << min_cost;
+
+    return 0;
+}
