c82db5ad5ae33508a91c3eec4c35faec4c27b81d
Lee-Sunho <sunho9889@naver.com>
2026-05-06 14:11:45 +0900

    같은 숫자는 싫어

---
+++ Programmers/같은 숫자는 싫어.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++) {
+        if (answer.empty()) {
+            answer.push_back(arr[i]);
+        } else if (answer[answer.size() - 1] != arr[i]) {
+            answer.push_back(arr[i]);
+        }
+    }
+
+    return answer;
+}
